e158080fafe11d927ee61b089ccca13eb02cec35
konstantin-rem <62418322+konstantin-rem@users.noreply.github.com>
2026-09-14 11:27:50 +0300

    Update noto_24.c

--- src/fonts/noto_24.c
+++ src/fonts/noto_24.c
@@ -4548,9 +4548,9 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
 
 /*Initialize a public general font descriptor*/
 #if LVGL_VERSION_MAJOR >= 8
-const lv_font_t LV_FONT_t NOTO_24 = {
+const lv_font_t LV_FONT_NOTO_24 = {
 #else
-lv_font_t lv_font_t NOTO_24 = {
+lv_font_t lv_font_t_NOTO_24 = {
 #endif
     .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
     .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/

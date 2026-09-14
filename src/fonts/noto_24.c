f7912b135b8558fd60aa60c0444b719fb369fd61
konstantin-rem <62418322+konstantin-rem@users.noreply.github.com>
2026-09-14 10:12:33 +0300

    Update noto_24.c

--- src/fonts/noto_24.c
+++ src/fonts/noto_24.c
@@ -4,18 +4,10 @@
  * Opts: --bpp 4 --size 24 --no-compress --stride 1 --align 1 --font NotoSans-Regular.ttf --range 32-127,160-255,1040-1103,1025,1105 --format lvgl -o NOTO_24.c
  ******************************************************************************/
 
-#ifdef __has_include
-    #if __has_include("lvgl.h")
-        #ifndef LV_LVGL_H_INCLUDE_SIMPLE
-            #define LV_LVGL_H_INCLUDE_SIMPLE
-        #endif
-    #endif
-#endif
-
 #ifdef LV_LVGL_H_INCLUDE_SIMPLE
-    #include "lvgl.h"
+#include "lvgl.h"
 #else
-    #include "lvgl/lvgl.h"
+#include "lvgl.h"
 #endif
 
 
@@ -4556,9 +4548,9 @@ static lv_font_fmt_txt_dsc_t font_dsc = {
 
 /*Initialize a public general font descriptor*/
 #if LVGL_VERSION_MAJOR >= 8
-const LV_FONT__t NOTO_24 = {
+const lv_font_t LV_FONT__t NOTO_24 = {
 #else
-lv_font_t NOTO_24 = {
+lv_font_t lv_font_t NOTO_24 = {
 #endif
     .get_glyph_dsc = lv_font_get_glyph_dsc_fmt_txt,    /*Function pointer to get glyph's data*/
     .get_glyph_bitmap = lv_font_get_bitmap_fmt_txt,    /*Function pointer to get glyph's bitmap*/
